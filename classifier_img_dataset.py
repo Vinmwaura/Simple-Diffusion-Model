7f7766267513ec89fc7f4ca5c70470a7739a0f4b
vincent <vinmwega@gmail.com>
2023-07-05 13:01:11 +0300

    Minor improvement to dataset and degrader files

--- classifier_img_dataset.py
+++ classifier_img_dataset.py
@@ -1,26 +1,27 @@
 import os
 import json
-
+import random
 
 import cv2
+import numpy as np
 
 import torch
+import torch.nn.functional as F
 from torch.utils.data import Dataset
-
 
 from tinydb import TinyDB
 
+from utils import *
+
 
 """
-Custom Image Loader using Opencv2 + Multi Output Classification.
+Custom Image Loader using Opencv2 + TinyDB(NoSQL) to load images and their classification.
 """
 class ClassifierImgDataset(Dataset):
-    def __init__(self, dataset_path=None):
-        # Path to images + Labels.
-        self.dataset_path = dataset_path
-
-        dataset_db = TinyDB(self.dataset_path)
-
+    def __init__(
+            self,
+            dataset_path=None):
+        dataset_db = TinyDB(dataset_path)
         data_tbl = dataset_db.table("Data")
         assert len(data_tbl) > 0
 
@@ -28,9 +29,12 @@ class ClassifierImgDataset(Dataset):
         assert len(labels_tbl) > 0
 
         self.all_labels = labels_tbl.all()[0]["labels"]
-        
+        self.all_data = data_tbl.all()
+        # self.all_data = data_tbl.search((where("k_means_41")==1) | (where("k_means_65")==1))
+        random.shuffle(self.all_data)
+
         self.dataset = []
-        for data in data_tbl.all():
+        for data in self.all_data:
             out_labels = []
             for label in self.all_labels:
                 out_labels.append(float(data[label]))
@@ -41,24 +45,25 @@ class ClassifierImgDataset(Dataset):
 
     def __len__(self):
         return len(self.dataset)
-    
+
     def __getitem__(self, index):
         assert len(self.dataset) > 0
 
         img_path, img_label = self.dataset[index]
-
+        
+        # Load images using opencv2.
+        img = cv2.imread(img_path)
+        
         # Convert labels to Tensor.
         labels_tensor = torch.Tensor(img_label)
 
-        # Load images using opencv2.
-        img = cv2.imread(img_path)
-              
         # Scale images to be between 1 and -1.
         img = (img.astype(float) - 127.5) / 127.5
 
         # Convert image as numpy to Tensor.
         img_tensor = torch.from_numpy(img).float()
-        
+
         # Permute image to be of format: [C,H,W]
         img_tensor = img_tensor.permute(2, 0, 1)
+
         return img_tensor, labels_tensor
--- degraders.py
+++ degraders.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torchvision
 
 
+# Linear Noise Degradation as detailed in the paper: Denoising Diffusion Probabilistic Models.
 class NoiseDegradation(nn.Module):
     def __init__(
             self,
@@ -18,16 +19,30 @@ class NoiseDegradation(nn.Module):
         self.beta_1 = beta_1
         self.beta_T = beta_T
         self.max_noise_step = max_noise_step
-        
+        """
+        Creates a one-dimensional tensor of size steps whose values are evenly spaced from start to end, inclusive e.g
+        torch.linspace(3, 10, steps=5) # tensor([  3.0000,   4.7500,   6.5000,   8.2500,  10.0000])
+        """
         self.beta = torch.linspace(
             start=self.beta_1,
             end=self.beta_T,
             steps=int(self.max_noise_step + 1),
             device=device)
         self.alpha = 1 - self.beta
+
+        """
+        Returns the cumulative product of elements of input in the dimension dim. e.g
+        tensor([ 0.6001,  0.2069, -0.1919,  0.9792,  0.6727,  1.0062,  0.4126, -0.2129, -0.4206,  0.1968])
+        =>
+        tensor([ 0.6001,  0.1241, -0.0238, -0.0233, -0.0157, -0.0000, -0.0000, 0.0000, -0.0000, -0.0000])
+        1) 0.6001 * 0.2069 = 0.12416069
+        2) 0.12416069 * -0.1919 = -0.023826436
+        ...
+        """
         self.alpha_cumulative_prod = torch.cumprod(self.alpha, dim=0)
 
     def get_timestep_params(self, step):
+        # Gathers values along an axis specified by dim.
         beta = torch.gather(self.beta, dim=0, index=step)
         alpha = torch.gather(self.alpha, dim=0, index=step)
         alpha_bar = torch.gather(self.alpha_cumulative_prod, dim=0, index=step)
@@ -44,7 +59,7 @@ class NoiseDegradation(nn.Module):
         return img_degradation
 
 
-# Cosine Scheduler
+# Cosine Noise Scheduler as detailed in the paper: Improved Denoising Diffusion Probabilistic Models.
 class CosineNoiseDegradation(nn.Module):
     def __init__(self, max_noise_step=1000):
         super().__init__()
