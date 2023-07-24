6693daf222b17b196f367aba65c7e8626e48e281
vincent <vinmwega@gmail.com>
2023-07-24 03:37:55 +0300

    Updated model unittest to be able to run without using too much RAM

--- tests/test_u_net_model.py
+++ tests/test_u_net_model.py
@@ -7,9 +7,17 @@ from models.U_Net import U_Net
 class TestUNetModel(unittest.TestCase):
     def setUp(self):
         self.u_net = U_Net()
-        self.x_T = torch.randn(16, 3, 128, 128)
-        self.time_step = torch.tensor([1000], dtype=torch.long)
+        
+        self.num_data = 1
+        self.img_C = 3
+        self.img_H = 128
+        self.img_W = 128
+
+        self.time_val = 1_000
+
+        self.x_T = torch.randn(self.num_data, self.img_C, self.img_H, self.img_W)
+        self.time_step = torch.tensor([self.time_val], dtype=torch.long)
     
     def test_unet_forward_pass(self):
         x_0 = self.u_net(self.x_T, self.time_step)
-        self.assertEqual(x_0.shape, (16, 3, 128, 128))
+        self.assertEqual(x_0.shape, (self.num_data, self.img_C, self.img_H, self.img_W))
